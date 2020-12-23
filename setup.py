2510f32c21f3910ce7a0f72828d8136a94f97b43
Jordan Eremieff <jordan@eremieff.com>
2020-12-24 01:56:14 +1100

    0.0.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description():
 
 setup(
     name="pydantic-django",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     license="MIT",
     url="https://github.com/jordaneremieff/pydantic-django/",
