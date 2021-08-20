01b3d42c7fef821bd922a38e420b9ecaf8c5ae9c
Jordan Eremieff <1376648+jordaneremieff@users.noreply.github.com>
2021-08-21 01:23:03 +1000

    Version 0.3.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description():
 
 setup(
     name="djantic",
-    version="0.3.4",
+    version="0.3.5",
     packages=find_packages(),
     license="MIT",
     url="https://github.com/jordaneremieff/djantic/",
